7b0035caa373a140d97804dd19369273c1069497
boouee <104202645+boouee@users.noreply.github.com>
2024-09-02 18:00:40 +0600

    Update index.php

--- api/index.php
+++ api/index.php
@@ -8,8 +8,8 @@
  */
 
 //Import PHPMailer classes into the global namespace
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
+//use PHPMailer\PHPMailer\PHPMailer;
+//use PHPMailer\PHPMailer\SMTP;
 
 require (__DIR__.'/PHPMailer.php');
 require (__DIR__.'/SMTP.php');
@@ -68,13 +68,13 @@ $mail->Subject = 'PHPMailer GMail SMTP test';
 
 //Read an HTML message body from an external file, convert referenced images to embedded,
 //convert HTML into a basic plain-text alternative body
-$mail->msgHTML(file_get_contents('contents.html'), __DIR__);
+$mail->msgHTML("hello");
 
 //Replace the plain text body with one created manually
 $mail->AltBody = 'This is a plain-text message body';
 
 //Attach an image file
-$mail->addAttachment('images/phpmailer_mini.png');
+//$mail->addAttachment('images/phpmailer_mini.png');
 
 //send the message, check for errors
 if (!$mail->send()) {
