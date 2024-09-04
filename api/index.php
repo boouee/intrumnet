8b514fc72c4c21a85fa54ff7f735e20a722880e6
boouee <104202645+boouee@users.noreply.github.com>
2024-09-04 15:51:03 +0600

    Update index.php

--- api/index.php
+++ api/index.php
@@ -16,6 +16,25 @@ require (__DIR__.'/SMTP.php');
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 //Create a new PHPMailer instance
+
+/* Handle CORS */
+
+// Specify domains from which requests are allowed
+header('Access-Control-Allow-Origin: *');
+
+// Specify which request methods are allowed
+header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS');
+
+// Additional headers which may be sent along with the CORS request
+header('Access-Control-Allow-Headers: X-Requested-With,Authorization,Content-Type');
+
+// Set the age to 1 day to improve speed/caching.
+header('Access-Control-Max-Age: 86400');
+
+// Exit early so the page isn't fully loaded for options requests
+if (strtolower($_SERVER['REQUEST_METHOD']) == 'options') {
+    exit();
+}
 $mail = new PHPMailer();
 
 //Tell PHPMailer to use SMTP
